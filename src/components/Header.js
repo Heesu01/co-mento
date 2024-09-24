b4671411a23765b21311a8dee4bc93c10df4f465
Heesu01 <rulru0413@naver.com>
2024-09-24 14:31:34 +0900

    feat: 헤더 라우터 연결

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <Container>
       <Left>
-        <Logo>Co-Mento</Logo>
+        <Logo onClick={() => navigate("/")}>Co-Mento</Logo>
         <Menu>
-          <Item>문제</Item>
-          <Item>문제집</Item>
-          <Item>랭킹</Item>
-          <Item>게시판</Item>
+          <Item onClick={() => navigate("/problemlist")}>문제</Item>
+          <Item onClick={() => navigate("/book")}>문제집</Item>
+          <Item onClick={() => navigate("/rank")}>랭킹</Item>
+          <Item onClick={() => navigate("/board")}>게시판</Item>
         </Menu>
       </Left>
       <BtnBox>
-        <Login>로그인</Login>
-        <Join>회원가입</Join>
+        <Login onClick={() => navigate("/auth/login")}>로그인</Login>
+        <Join onClick={() => navigate("/auth/join")}>회원가입</Join>
       </BtnBox>
     </Container>
   );
