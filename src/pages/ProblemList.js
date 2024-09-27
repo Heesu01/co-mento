dec6cc9465ea575e5a0a0c1dca18cdca3e40a89c
Heesu01 <rulru0413@naver.com>
2024-09-27 20:57:52 +0900

    feat: 알고리즘 분류 UI 구현

--- src/pages/ProblemList.js
+++ src/pages/ProblemList.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { FaCheck } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 const ProblemList = () => {
   const navigate = useNavigate();
-
   const problems = [
     {
       id: 1,
@@ -80,6 +79,56 @@ const ProblemList = () => {
       success: true,
     },
   ];
+  const data = [
+    "수학",
+    "구현",
+    "그리디 알고리즘",
+    "다이나믹",
+    "정렬",
+    "문자열",
+  ];
+  const [algorithmChoseBox, setAlgorithmChoseBox] = useState(false);
+  const [inputAlgorithm, setInputAlgorithm] = useState("");
+  const [algorithm, setAlgorithm] = useState([]);
+
+  const AlgorithmInputClick = () => {
+    setAlgorithmChoseBox(!algorithmChoseBox);
+  };
+
+  const ChangeAlgorithm = (e) => {
+    setInputAlgorithm(e.target.value);
+  };
+
+  const AlgorithmClick = (name) => {
+    setAlgorithm((prevAlgorithm) => {
+      if (!prevAlgorithm.includes(name)) {
+        return [...prevAlgorithm, name];
+      } else {
+        return prevAlgorithm;
+      }
+    });
+  };
+
+  const DeleteAlgorithm = (name) => {
+    setAlgorithm((prevAlgorithm) =>
+      prevAlgorithm.filter((algorithms) => algorithms !== name)
+    );
+  };
+
+  const filteredData = data.filter((item) =>
+    item.toLowerCase().includes(inputAlgorithm.toLowerCase())
+  );
+
+  const [difficulty, setDifficulty] = useState("");
+  const [successStatus, setSuccessStatus] = useState("");
+
+  const handleDifficultyChange = (value) => {
+    setDifficulty(value);
+  };
+
+  const handleSuccessStatusChange = (value) => {
+    setSuccessStatus(value);
+  };
 
   return (
     <Container>
@@ -87,7 +136,92 @@ const ProblemList = () => {
         <Input placeholder="문제를 검색해주세요."></Input>
         <SearchBtn>검색</SearchBtn>
       </SearchBox>
-      <SpecificBox></SpecificBox>
+      <SpecificBox>
+        <p>알고리즘 분류</p>
+        <AlgorithmListBox>
+          {algorithm.map((algorithms, index) => (
+            <LiBox>
+              <AlgorithmList key={index}>{algorithms}</AlgorithmList>
+              <Delete onClick={() => DeleteAlgorithm(algorithms)}>X</Delete>
+            </LiBox>
+          ))}
+        </AlgorithmListBox>
+        <AlgorithmInputBox
+          placeholder="알고리즘"
+          value={inputAlgorithm}
+          onClick={AlgorithmInputClick}
+          onChange={(e) => ChangeAlgorithm(e)}
+        />
+        {algorithmChoseBox && (
+          <>
+            <ChoseBox>
+              {filteredData.length > 0 &&
+                filteredData.map((datas, index) => (
+                  <Algorithm key={index} onClick={() => AlgorithmClick(datas)}>
+                    {datas}
+                  </Algorithm>
+                ))}
+            </ChoseBox>
+          </>
+        )}
+        <BottomBox>
+          <LeftBox>
+            <CheckBoxs>
+              <p>난이도</p>
+              <CheckBoxGroup>
+                <CustomCheckbox
+                  label="쉬움"
+                  value="쉬움"
+                  isChecked={difficulty === "쉬움"}
+                  onChange={() => handleDifficultyChange("쉬움")}
+                />
+                <CustomCheckbox
+                  label="보통"
+                  value="보통"
+                  isChecked={difficulty === "보통"}
+                  onChange={() => handleDifficultyChange("보통")}
+                />
+                <CustomCheckbox
+                  label="어려움"
+                  value="어려움"
+                  isChecked={difficulty === "어려움"}
+                  onChange={() => handleDifficultyChange("어려움")}
+                />
+              </CheckBoxGroup>
+            </CheckBoxs>
+            <CheckBoxs>
+              <p>성공 여부</p>
+              <CheckBoxGroup>
+                <CustomCheckbox
+                  label="성공"
+                  value="성공"
+                  isChecked={successStatus === "성공"}
+                  onChange={() => handleSuccessStatusChange("성공")}
+                />
+                <CustomCheckbox
+                  label="실패"
+                  value="실패"
+                  isChecked={successStatus === "실패"}
+                  onChange={() => handleSuccessStatusChange("실패")}
+                />
+                <CustomCheckbox
+                  label="미도전"
+                  value="미도전"
+                  isChecked={successStatus === "미도전"}
+                  onChange={() => handleSuccessStatusChange("미도전")}
+                />
+              </CheckBoxGroup>
+            </CheckBoxs>
+          </LeftBox>
+          <RightBox>
+            <ButtonBox>
+              <ApplyBtn>적용</ApplyBtn>
+              <ResetBtn>초기화</ResetBtn>
+              <RandomBtn>랜덤</RandomBtn>
+            </ButtonBox>
+          </RightBox>
+        </BottomBox>
+      </SpecificBox>
       <ListBox>
         <Top>
           <Num>문제번호</Num>
@@ -136,12 +270,145 @@ const SearchBtn = styled.button`
   border: 1px solid ${({ theme }) => theme.colors.gray2};
   font-weight: 500;
 `;
+
 const SpecificBox = styled.div`
   width: 100%;
-  height: 200px;
+  height: auto;
   border: 1px solid ${({ theme }) => theme.colors.gray2};
   padding: 20px 35px;
 `;
+const AlgorithmListBox = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-bottom: 10px;
+`;
+const LiBox = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 10px;
+  background-color: ${({ theme }) => theme.colors.deepPink};
+  padding: 3px 7px 3px 10px;
+  border-radius: 8px;
+`;
+const AlgorithmList = styled.div`
+  font-size: 13px;
+`;
+const Delete = styled.p`
+  margin-left: 10px;
+  font-size: 11px;
+  cursor: pointer;
+`;
+const AlgorithmInputBox = styled.input`
+  width: 100%;
+  padding: 5px 10px;
+  outline: none;
+  border: 1px solid ${({ theme }) => theme.colors.gray2};
+`;
+const ChoseBox = styled.div`
+  width: 100%;
+  height: 130px;
+  background-color: white;
+  overflow-y: scroll;
+  border-left: 1px solid ${({ theme }) => theme.colors.gray2};
+  border-right: 1px solid ${({ theme }) => theme.colors.gray2};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.gray2};
+  padding-top: 3px;
+`;
+const Algorithm = styled.p`
+  font-size: 14px;
+  margin-bottom: 6px;
+  padding: 3px 10px;
+  &:hover {
+    background-color: ${({ theme }) => theme.colors.gray3};
+  }
+`;
+const BottomBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 60px;
+  align-items: center;
+  margin-top: 10px;
+`;
+const LeftBox = styled.div`
+  display: flex;
+  gap: 40px;
+`;
+const CheckBoxs = styled.div`
+  p {
+    margin-bottom: 10px;
+  }
+`;
+const CheckBoxGroup = styled.div`
+  display: flex;
+  gap: 10px;
+`;
+const CustomCheckbox = ({ label, value, isChecked, onChange }) => {
+  return (
+    <CheckboxWrapper>
+      <HiddenCheckbox type="checkbox" checked={isChecked} onChange={onChange} />
+      <StyledCheckbox isChecked={isChecked} onClick={onChange}>
+        {isChecked && <CheckMark />}
+      </StyledCheckbox>
+      <span>{label}</span>
+    </CheckboxWrapper>
+  );
+};
+const CheckboxWrapper = styled.label`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+  font-size: 14px;
+`;
+const HiddenCheckbox = styled.input`
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+  height: 0;
+  width: 0;
+`;
+const StyledCheckbox = styled.div`
+  width: 12px;
+  height: 12px;
+  background-color: ${({ isChecked, theme }) =>
+    isChecked ? theme.colors.gray : theme.colors.white};
+  border: 1px solid ${({ theme }) => theme.colors.gray};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const CheckMark = styled.div`
+  width: 8px;
+  height: 8px;
+`;
+const RightBox = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: end;
+`;
+const ButtonBox = styled.div`
+  display: flex;
+  gap: 7px;
+`;
+const ApplyBtn = styled.button`
+  padding: 3px 8px;
+  font-size: 12px;
+  background-color: ${({ theme }) => theme.colors.blue};
+  color: white;
+`;
+const ResetBtn = styled.button`
+  padding: 3px 8px;
+  font-size: 12px;
+  background-color: ${({ theme }) => theme.colors.red};
+  color: white;
+`;
+const RandomBtn = styled.button`
+  padding: 3px 8px;
+  font-size: 12px;
+  background-color: ${({ theme }) => theme.colors.yellow};
+  color: white;
+`;
+
 const ListBox = styled.div`
   width: 100%;
   height: 800px;
