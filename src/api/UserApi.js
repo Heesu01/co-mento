cf44084bedfc938ac8858d6dcffed0a2199fdab5
Heesu01 <rulru0413@naver.com>
2024-11-04 03:08:28 +0900

    feat: 제출목록 api 연결

--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -4,3 +4,17 @@ export const fetchUserProfile = async (userProfileId) => {
   const response = await Axios.get(`/users/${userProfileId}`);
   return response.data;
 };
+
+export const fetchUserSolutions = async (userProfileId) => {
+  const response = await Axios.get(`/solutions?${userProfileId}`);
+  return response.data.data.solutionList.map((solution) => ({
+    id: solution.id,
+    solutionId: solution.id,
+    num: solution.problemId,
+    title: solution.problemTitle,
+    language: solution.language,
+    submitter: solution.userName,
+    subtime: solution.timeAgo,
+    success: solution.correct,
+  }));
+};
