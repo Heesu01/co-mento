c037d086fbc3b14b60a9dac07ebf6651d74a3300
Heesu01 <rulru0413@naver.com>
2024-11-30 03:08:35 +0900

    feat: 메인 진행도 api 연결

--- src/api/UserApi.js
+++ src/api/UserApi.js
@@ -2,21 +2,28 @@ import { Axios } from "./Api";
 
 export const fetchUserProfile = async (userProfileId) => {
   const response = await Axios.get(`/users/${userProfileId}`);
-  return response.data;
+  return response.data.data;
 };
 
-export const fetchUserSolutions = async (userProfileId) => {
-  const response = await Axios.get(`/solutions?${userProfileId}`);
-  return response.data.data.solutionList.map((solution) => ({
-    id: solution.id,
-    solutionId: solution.id,
-    num: solution.problemId,
-    title: solution.problemTitle,
-    language: solution.language,
-    submitter: solution.userName,
-    subtime: solution.timeAgo,
-    success: solution.correct,
-  }));
+export const fetchUserSolutions = async (userProfileId, page) => {
+  const response = await Axios.get(`/solutions`, {
+    params: { userProfileId, page: page - 1 },
+  });
+
+  const { solutionList, paginationResponse } = response.data.data;
+  return {
+    solutions: solutionList.map((solution) => ({
+      id: solution.id,
+      solutionId: solution.id,
+      num: solution.problemId,
+      title: solution.problemTitle,
+      language: solution.language,
+      submitter: solution.userName,
+      subtime: solution.timeAgo,
+      success: solution.correct,
+    })),
+    totalPages: paginationResponse.totalPage,
+  };
 };
 
 export const fetchSolutionDetail = async (solutionId) => {
@@ -27,4 +34,11 @@ export const fetchSolutionDetail = async (solutionId) => {
     console.error("문제 상세조회 에러:", error);
     throw error;
   }
+};
+
+export const fetchCollectionProgress = async () => {
+  const response = await Axios.get(`/problems/collections/progress`);
+  return response.data.data.collectionProgresses.filter(
+    (collection) => collection.progress > 0
+  );
 };
